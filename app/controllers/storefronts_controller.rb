c92c517ba31df4cf303dc12b55e1c65b3b7c432a
morningowl <salazarcarlosno9@gmail.com>
2019-05-28 11:11:47 -0500

    create items by category view

--- app/controllers/storefronts_controller.rb
+++ app/controllers/storefronts_controller.rb
@@ -2,6 +2,7 @@ class StorefrontsController < ApplicationController
 
 	def all_items
 	    @products = Product.all
+	  
 	end
 	def items_by_category
 		@products = Product.where(category_id: params[:category_id]) #select products where category_id = 
